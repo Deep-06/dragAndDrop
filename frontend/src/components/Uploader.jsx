9ba4fb109a7fafb8ff2bbf50b6c177d02f1ca4b9
Deep-06 <shikhadeep.hzp@gmail.com>
2024-02-29 13:30:10 +0530

    upadate

--- frontend/src/components/Uploader.jsx
+++ frontend/src/components/Uploader.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import axios from 'axios'
 export const Uploader = () => {
     const [image,setImage]=useState([]);
     const [error,setError]=useState('');
@@ -7,30 +7,42 @@ export const Uploader = () => {
 
 
 const handleInput=async(e)=>{
-    const file=e.target.
+    const file=e.target.file[0]
+    await handleUpload(file)
 }
 
 const handleUpload=async(file)=>{
 try{
 const data=new FormData()
 data.append('image',file)
-const res=await axios.post('/upload',data,{
+const res=await axios.post('http://localhost:8080/upload',data,{
     headers:{
         'Content-type':"multipart/form-data"
     }
 })
+
 const {url}=res.data
 setImage([...image,url])
 }catch(err){
     console.log(err);
+    setError('Error')
 }
 }
   return (
     <div>
-        <div >
+        <div style={{padding:'40px'}}>
             Drop Image here
         </div>
-        <input placeholder='select image'/>
+        <input type='file' 
+        onChange={handleInput}
+        placeholder='select image'/>
+
+        {error && <p>{error}</p>}
+        <div>
+            {image.map((e,i)=>(
+                <img key={i} src={e} alt='img'/>
+            ))}
+        </div>
     </div>
   )
 }
